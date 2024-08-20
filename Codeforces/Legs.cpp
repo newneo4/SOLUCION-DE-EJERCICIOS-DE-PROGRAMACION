3bd6edb90e43867586b624c967f5c203939debfd
newneo4 <noel123mch@gmail.com>
2024-08-20 01:35:49 -0500

    m day#

--- Codeforces/Legs.cpp
+++ Codeforces/Legs.cpp
@@ -1,18 +1,25 @@
-//https://codeforces.com/contest/1996/problem/A
+//https://codeforces.com/problemset/problem/1996/A
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
+#define fastIO ios::sync_with_stdio(0); cin.tie(0);
+#define vii vector<int>
+#define pb push_back
+#define ll long long
+
 int main(){
-    int t = 0, n = 0;
     
-    cin>>t;
+    fastIO;
+    
+    int t; cin>>t;
     
     while(t--){
-        cin>>n;
-        int ans = !(n % 4) ? n/4 : ((n/4) + ((n%4) / 2));
-        cout<<ans<<'\n';
         
+        int n; cin>>n;
+        
+        cout<<(n/4) + (n%4 != 0)<<'\n';
     }
     
     return 0;
